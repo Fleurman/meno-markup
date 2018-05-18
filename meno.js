38d66be62f5845b9e80fae909662720e238875cb
Martin Ashton-Lomax <ashtonlomaxmartin@gmail.com>
2018-05-18 16:01:01 +0200

    V 1.0

--- meno.js
+++ meno.js
@@ -1,5 +1,5 @@
 var meno = (function () {
-	var m = { version: "0.9.99" };
+	var m = { version: "1.0" };
 	m.checkUpdates = function(){
 		loadFile(function (js) {
 			try {
@@ -505,7 +505,7 @@ var meno = (function () {
 		return inner;
 	};
 
-	m.css= ".meno{font-family:Calibri}.meno code{display:block;background-color:#f5f5f5;color:#696969;font-family:consolas}.meno samp{background-color:#fdfdfd;color:grey;font-variant:petite-caps}.meno textarea{resize:none}.meno details{border-bottom:2px dotted #d3d3d3;border-right:2px dotted #d3d3d3}.meno details span{color:grey;font-size:.9em;padding-left:20px;display:block}.meno [-inblock],.meno nav{display:inline-block}.meno h1,h2,h3,h4,h5,h6{margin:4px auto}.meno a{color:#87ceeb;text-decoration:none;border-bottom:1px solid #d3d3d3}.meno nav{margin:5px;padding:0 5px;border-right:2px solid #d3d3d3;border-left:2px solid #d3d3d3;background-color:#fafafa}.meno nav a{border:none}.meno blockquote{font-style:italic;margin-left:1em}.meno blockquote::before{content:open-quote}.meno blockquote::after{content:close-quote}.meno [-list] span{margin-left:15px;font-size:.8em;color:#696969}.meno [-list]{padding:5px;border-left:2px solid #d3d3d3;list-style-position:inside}.meno [-list] [-list]{border:none;padding:2px;margin-left:20px}.meno [-list]>li{font-size:1.2em;font-weight:700}.meno ul[-list]>li{list-style-type:none}.meno ul[-list]>li:before{content:'| '}.meno [-list] [-list]>li{font-size:1.1em;color:#696969}.meno [-list] ul[-list]>li:before{content:': ';color:#000}.meno [-list] [-list] [-list]>li{font-size:1.05em;font-weight:400;color:grey}.meno [-list] [-list] ul[-list]>li:before{content:'· '}.meno [-list] [-list] [-list] [-list]>li{font-size:1em;font-weight:400;color:#000}.meno [-list] [-list] [-list] ul[-list]>li:before{content:'- ';color:#000}.meno [-hr]{height:2px;margin:1em 0}.meno [-col] p{margin-top:0}.meno [-tip]{position:relative;display:inline;border-bottom:1px dotted pink}.meno [-tip]::after{content:attr(-tip);position:absolute;bottom:100%;left:50%;opacity:0;visibility:hidden;transition:opacity .3s;font-size:13px;min-width:100px;background-color:rgba(50,50,50,.8);color:#fff;border-bottom:2px solid orange;border-radius:10px 0;padding:3px 8px}.meno [-tip]:hover::after{opacity:1;visibility:visible}.meno [-tip]:hover{border:1px solid orange;margin:-1px;background-color:rgba(250,250,100,.1);cursor:help}.meno ::selection{background-color:orange;color:#fff}",
+	m.css= ".meno{font-family:Calibri}.meno code{display:block;background-color:#f5f5f5;color:#696969;font-family:consolas}.meno samp{background-color:#fdfdfd;color:grey;font-variant:petite-caps}.meno details{border-bottom:2px dotted #d3d3d3;border-right:2px dotted #d3d3d3}.meno details span{color:grey;font-size:.9em;padding-left:20px;display:block}.meno [-inblock],.meno nav{display:inline-block}.meno h1,.meno h2,.meno h3,.meno h4,.meno h5,.meno h6{margin:4px auto}.meno a{color:#87ceeb;text-decoration:none;border-bottom:1px solid #d3d3d3}.meno nav{margin:5px;padding:0 5px;border-right:2px solid #d3d3d3;border-left:2px solid #d3d3d3;background-color:#fafafa}.meno nav a{border:none}.meno blockquote{font-style:italic;margin-left:1em}.meno blockquote::before{content:open-quote}.meno blockquote::after{content:close-quote}.meno [-list] span{margin-left:15px;font-size:.8em;color:#696969}.meno [-list]{padding:5px;border-left:2px solid #d3d3d3;list-style-position:inside}.meno [-list] [-list]{border:none;padding:2px;margin-left:20px}.meno [-list]>li{font-size:1.2em;font-weight:700}.meno ul[-list]>li{list-style-type:none}.meno ul[-list]>li:before{content:'| '}.meno [-list] [-list]>li{font-size:1.1em;color:#696969}.meno [-list] ul[-list]>li:before{content:': ';color:#000}.meno [-list] [-list] [-list]>li{font-size:1.05em;font-weight:400;color:grey}.meno [-list] [-list] ul[-list]>li:before{content:'· '}.meno [-list] [-list] [-list] [-list]>li{font-size:1em;font-weight:400;color:#000}.meno [-list] [-list] [-list] ul[-list]>li:before{content:'- ';color:#000}.meno [-hr]{height:2px;margin:1em 0}.meno [-col] p{margin-top:0}.meno [-tip]{position:relative;display:inline;border-bottom:1px dotted pink}.meno [-tip]::after{content:attr(-tip);position:absolute;bottom:100%;left:50%;opacity:0;visibility:hidden;transition:opacity .3s;font-size:13px;min-width:100px;background-color:rgba(50,50,50,.8);color:#fff;border-bottom:2px solid orange;border-radius:10px 0;padding:3px 8px}.meno [-tip]:hover::after{opacity:1;visibility:visible}.meno [-tip]:hover{border:1px solid orange;margin:-1px;background-color:rgba(250,250,100,.1);cursor:help}.meno ::selection{background-color:orange;color:#fff}",
 
 	m.addCSS= function () {
 		var styl = document.createElement("style");
@@ -536,6 +536,14 @@ var meno = (function () {
 			menos[i].parentNode.insertBefore(art, menos[i]);
 			art.innerHTML = m.parsed(raw);
 			menos[i].parentNode.removeChild(menos[i]);
+		}
+	}
+	m.load= function () {
+		var menos = document.querySelectorAll("[meno]");
+		for (var i = 0; i < menos.length; i++) {
+			var file = menos[i].getAttribute("meno");
+			m.loadTo(menos[i],file);
+			menos[i].className += " meno";
 		}
 	}
 	return m;
